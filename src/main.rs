ce75931a97c7a2ff7e05fa3220b5e2d68fa1013f
john <john@cariboujohn.net>
2024-03-10 00:30:26 +0000

    numeric decode

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use hex::encode;
 use serde_json;
 use std::env;
 
@@ -7,14 +8,24 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
+
     if encoded_value.chars().next().unwrap().is_digit(10) {
         // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
+        let colon_index = encoded_value.find('i').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<i64>().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
         return serde_json::Value::String(string.to_string());
-    } else {
+    } 
+    else if encoded_value.chars().next().unwrap() == 'i'
+    {
+        let e_index = encoded_value.find('e').unwrap();
+        let number_string = &encoded_value[1..e_index];
+        let number = number_string.parse().unwrap(); 
+        return serde_json::Value::Number(number);
+    } 
+    else
+    {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
 }
